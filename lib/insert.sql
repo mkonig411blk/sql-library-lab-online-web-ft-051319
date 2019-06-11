0904d6ff9bbd5ee1e5cf3cb0d073cd85bb01cc63
Margot Konig <margot.konig@blackrock.com>
2019-06-11 04:22:45 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,5 +5,5 @@ INSERT INTO subgenres (name) VALUES ("Text2");
 INSERT INTO authors (name) VALUES ("Text2");
 INSERT INTO authors (name) VALUES ("Text2");
 INSERT INTO books (title, year, series_id) VALUES ("Text2", 1992, 1);
-INSERT INTO characters (title, year, series_id) VALUES ("Text2", 1992, 1);
+INSERT INTO characters (name, motto, species, author_id, ) VALUES ("Text2", 1992, 1);
 INSERT INTO books (title, year, series_id) VALUES ("Text2", 1992, 1);
