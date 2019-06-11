f6d07621d767481e3a5a7d0821d3424cec12d40c
Margot Konig <margot.konig@blackrock.com>
2019-06-11 04:53:09 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,9 +15,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "select species.name, count(*)
-      from species 
-      group by species.name"
+  "select characters.species, count(*)
+      from characters 
+      group by characters.species"
 end
 
 def select_name_and_series_subgenres_of_authors
