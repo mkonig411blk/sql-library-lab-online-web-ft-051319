727ba0131e6fe06952f453b32cf1418e59333278
Margot Konig <margot.konig@blackrock.com>
2019-06-11 04:33:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,7 +4,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
       left join series 
       ON books.series_id = series.id
       where series.id = 1 
-      order by books.year DESC"
+      order by books.year ASC"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
