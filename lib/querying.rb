132277fdaf85b8aac25ae1e6c3ad52057833ed1a
Margot Konig <margot.konig@blackrock.com>
2019-06-11 04:32:29 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -2,7 +2,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "select books.title, books.year 
       from books 
       left join on series 
-      "
+      ON books.id = cats_owners.owner_id"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
