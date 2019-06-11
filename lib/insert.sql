0744d0e15bd1ded5576c42f0d8e7c34a021ea3c0
Margot Konig <margot.konig@blackrock.com>
2019-06-11 04:29:31 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -24,6 +24,17 @@ INSERT INTO character_books (character_id, book_id) VALUES (1,3);
 INSERT INTO character_books (character_id, book_id) VALUES (2,1);
 INSERT INTO character_books (character_id, book_id) VALUES (2,2);
 INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (3,3);
+INSERT INTO character_books (character_id, book_id) VALUES (4,1);
+INSERT INTO character_books (character_id, book_id) VALUES (5,4);
+INSERT INTO character_books (character_id, book_id) VALUES (5,5);
+INSERT INTO character_books (character_id, book_id) VALUES (5,6);
+INSERT INTO character_books (character_id, book_id) VALUES (6,4);
+INSERT INTO character_books (character_id, book_id) VALUES (6,5);
+INSERT INTO character_books (character_id, book_id) VALUES (6,6);
+INSERT INTO character_books (character_id, book_id) VALUES (7,5);
+INSERT INTO character_books (character_id, book_id) VALUES (7,5);
+
 
 
 
