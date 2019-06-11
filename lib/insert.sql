60b5870faf322f594c9f66c39f0033ae38f6c7d3
Margot Konig <margot.konig@blackrock.com>
2019-06-11 04:28:17 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -19,5 +19,11 @@ INSERT INTO characters (name, motto, species, author_id, series_id ) VALUES ("da
 INSERT INTO characters (name, motto, species, author_id, series_id ) VALUES ("danny","run","human",2,2);
 INSERT INTO characters (name, motto, species, author_id, series_id ) VALUES ("danny","run","human",2,2);
 INSERT INTO character_books (character_id, book_id) VALUES (1,1);
+INSERT INTO character_books (character_id, book_id) VALUES (1,2);
+INSERT INTO character_books (character_id, book_id) VALUES (1,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,1);
+INSERT INTO character_books (character_id, book_id) VALUES (2,2);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+
 
 
