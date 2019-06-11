c94f90ba6ac270b685cb4e38cc7ed1cdd5dabeaa
Margot Konig <margot.konig@blackrock.com>
2019-06-11 04:48:14 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,5 +34,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "select characters.names, count(*)
+      from characters 
+      left join characters"
 end
