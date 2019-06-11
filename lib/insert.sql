507f00106cf43920a2685113932fdb3180c72c2b
Margot Konig <margot.konig@blackrock.com>
2019-06-11 04:21:17 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Text", 1, 2);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Text", 1, 2);
+INSERT INTO subgenres (name) VALUES ("Text");
