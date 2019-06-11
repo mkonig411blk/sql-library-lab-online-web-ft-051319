6c1550eefdd74bfb78c8e91a2afff99e624bb819
Margot Konig <margot.konig@blackrock.com>
2019-06-11 04:24:06 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("HP", 1, 2);
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Text", 1, 2);
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Text", 1, 2);
-INSERT INTO subgenres (name) VALUES ("Text");
-INSERT INTO subgenres (name) VALUES ("Text2");
-INSERT INTO authors (name) VALUES ("Text2");
+INSERT INTO subgenres (name) VALUES ("Fantasy");
+INSERT INTO subgenres (name) VALUES ("Magic");
+INSERT INTO authors (name) VALUES ("JK Rowling");
 INSERT INTO authors (name) VALUES ("Text2");
 INSERT INTO books (title, year, series_id) VALUES ("Text2", 1992, 1);
 INSERT INTO books (title, year, series_id) VALUES ("Text2", 1992, 1);
